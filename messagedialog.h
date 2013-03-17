ff110c94e635c3f522fad6e82f37990641bf4230
Oleg Garin <oleggrrus@gmail.com>
2013-03-17 16:20:05 +0300

    Create messagedialog.h

---
+++ messagedialog.h
@@ -0,0 +1,24 @@
+#ifndef MESSAGEDIALOG_H
+#define MESSAGEDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class MessageDialog;
+}
+
+class MessageDialog : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit MessageDialog(QWidget *parent = 0);
+    ~MessageDialog();
+    void setText(QString s);
+    
+private:
+    Ui::MessageDialog *ui;
+
+};
+
+#endif // MESSAGEDIALOG_H
